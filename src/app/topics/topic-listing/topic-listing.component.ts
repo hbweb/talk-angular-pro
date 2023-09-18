aa152b6b759497fa8c89daf3de5f0b082da9e499
Hung Bui <hung@quaycreative.com>
2023-09-18 11:28:18 +0700

    add topic listing

--- src/app/topics/topic-listing/topic-listing.component.ts
+++ src/app/topics/topic-listing/topic-listing.component.ts
@@ -1,4 +1,7 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import { of } from 'rxjs/internal/observable/of';
+import { Topic } from 'src/app/shared/models';
 
 @Component({
   selector: 'app-topic-listing',
@@ -6,5 +9,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./topic-listing.component.scss']
 })
 export class TopicListingComponent {
+
+  topics$: Observable<Topic[]> = of([
+    {"id": 1, "title": "Content Injection", "icon": "fa-solid fa-house","path": "/"},
+    {"id": 2, "title": "Dependency Injection", "icon": "fa-brands fa-facebook","path": "/"},
+    {"id": 3, "title": "Angular Architecture", "icon": "fa-solid fa-bomb","path": "/"},
+    {"id": 1, "title": "Content Injection", "icon": "fa-solid fa-house","path": "/"},
+    {"id": 2, "title": "Dependency Injection", "icon": "fa-brands fa-facebook","path": "/"},
+    {"id": 3, "title": "Angular Architecture", "icon": "fa-solid fa-bomb","path": "/"},
+    {"id": 1, "title": "Content Injection", "icon": "fa-solid fa-house","path": "/"},
+    {"id": 2, "title": "Dependency Injection", "icon": "fa-brands fa-facebook","path": "/"},
+    {"id": 3, "title": "Angular Architecture", "icon": "fa-solid fa-bomb","path": "/"}
+  ]);
 
 }
